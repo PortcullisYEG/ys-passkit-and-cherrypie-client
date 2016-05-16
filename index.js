e880763064fcdd7d55e06bde482fd9a12d7b4acf
Michal Cerny <michalcerny@Michal-MacBook-Pro.local>
2016-05-16 16:43:45 +0200

    version 0.0.4

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import 'babel-polyfill'
 import passkit from './lib/passkit';
 import cherryPie from './lib/cherryPie';
-
 export const  PasskitClient = passkit;
 export const CherryPieClient = cherryPie;
