d4e12c147d0c1dd23b7aee7fecb59f4ed44d852f
Michal Cerny <michalcerny@Michal-MacBook-Pro.local>
2016-05-16 14:51:07 +0200

    some cherry pie updates

--- test/cherrypie.spec.js
+++ test/cherrypie.spec.js
@@ -1,10 +1,8 @@
 import 'babel-polyfill';
 import {CherryPieClient} from '../index.js';
 import {expect} from 'chai';
-const TIMEOUT = 20000;
 import Bluebird from 'bluebird';
-
-
+const TIMEOUT = 20000;
 
 const options = {
   apiKey: process.env.CHERRYPIE_API_KEY,
@@ -14,20 +12,17 @@ const options = {
 const templateName = process.env.CHERRYPIE_TEST_OFFER;
 const recoveryEmail = process.env.CHERRYPIE_TEST_RECOVERY_EMAIL ? process.env.CHERRYPIE_TEST_RECOVERY_EMAIL : null;
 const client = new CherryPieClient(options);
-
-
 const userDefinedId = `USER_${new Date().getTime()}`;
-
 let passId = null;
 
-describe('test cherryPie', () => {
+describe(`CherryPie for templateName=${templateName} and recoveryEmail ${recoveryEmail}`, () => {
 
 
   it('create pass', function (done) {
     this.timeout(TIMEOUT);
     client.createPass(templateName, userDefinedId, {}, recoveryEmail, function (err, response) {
       if (err) {
-        console.error(err);
+        //console.error(err);
       } else {
         //console.log(response.body)
         passId = response.body.id;
@@ -43,24 +38,36 @@ describe('test cherryPie', () => {
       done();
     }, (err) => {
       {
-        console.error(err);
+        //console.error(err);
         done(err);
       }
     });
   });
 
-
-  it('get pass instance by passID', function (done) {
-    this.timeout(TIMEOUT);
-    client.getPass(passId, function (err, response) {
-      if (err) {
-        console.error(err);
-      } else {
-        //console.log(response);
-      }
-      done(err);
+  if (passId) {
+    it('get pass instance by passID', function (done) {
+      this.timeout(TIMEOUT);
+      client.getPass(passId, function (err, response) {
+        if (err) {
+          //console.error(err);
+        } else {
+          console.log(response);
+        }
+        done(err);
+      });
     });
-  });
 
-
+    it('get pass instance by passID async', function (done) {
+      this.timeout(TIMEOUT);
+      client.getPassAsync(passId).then((result) => {
+        passId = result.body.id;
+        done();
+      }, (err) => {
+        {
+          // console.error(err);
+          done(err);
+        }
+      });
+    });
+  }
 });
